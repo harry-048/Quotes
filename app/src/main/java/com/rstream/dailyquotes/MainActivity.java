1c9bd9e71fafcd5accdea1cb6ba3dffbfd2c52be
harry-048 <aravindjayan007@gmail.com>
2019-04-17 10:27:51 +0530

    push notifications started

--- app/src/main/java/com/rstream/dailyquotes/MainActivity.java
+++ app/src/main/java/com/rstream/dailyquotes/MainActivity.java
@@ -119,6 +119,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // MobileAds.initialize(this, "ca-app-pub-9098946909579213~1471105716");
 
+        /*FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });*/
 
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
